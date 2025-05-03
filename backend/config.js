0ce58b617724bddec3fe1074134b447edc77ffc5
Karl <auffenbergmedia@gmail.com>
2025-05-03 04:59:36 -0400

    Add PayPal config.js so captureOrder can import credentials

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+// backend/config.js
+
+// load your secrets from env-vars
+export default {
+    PAYPAL_CLIENT_ID:     process.env.PAYPAL_CLIENT_ID,
+    PAYPAL_CLIENT_SECRET: process.env.PAYPAL_CLIENT_SECRET,
+  };
